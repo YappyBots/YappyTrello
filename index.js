e707a6e90e6043ffb8d8d44d1222bef35d52e772
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-16 17:28:23 -0400

    Added mongoose models; added init command; fixed web listen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Log = require("./lib/Util/Log");
 
 let Web = require("./lib/Web");
 require("./lib/Discord");
+require("./lib/Models");
 
 process.on("unhandledRejection", Log.error);
 process.on("uncaughtException", Log.error);
